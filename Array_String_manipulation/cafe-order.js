fc8966a2fb85d2c5ff1823dac1c828115503a6f4
Chihiro Shibayama <chihiro.shibayama@gmail.com>
2023-02-13 22:23:53 -0800

    cafe order examples

--- Array_String_manipulation/cafe-order.js
+++ Array_String_manipulation/cafe-order.js
@@ -1,1 +1,9 @@
 // Given all three arrays, write a function to check that my service is first-come, first-served. All food should come out in the same order customers requested it.
+
+/*
+Correct examples: 
+
+Take Out Orders: [17, 8, 24]
+Dine In Orders: [12, 19, 2]
+Served Orders: [17, 8, 12, 19, 24, 2]
+ */
